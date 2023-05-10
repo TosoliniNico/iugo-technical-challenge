650ea553145ecc253a7b558cacb4249d3f366361
Nico Tosolini <nicotosolini@Nicos-MacBook-Pro.local>
2023-05-09 21:36:28 -0300

    feat(recursiveFibonacci): add dynamic fibonacci function and unit test

--- src/fibonacci/test/recursiveFibonacci.test.js
+++ src/fibonacci/test/recursiveFibonacci.test.js
@@ -1,25 +1,27 @@
 const recursiveFibonacci = require("../recursiveFibonacci");
 
-// test("Test for recursive fibonacci", () => {
-//   expect(recursiveFibonacci(0)).toBe(0);
-//   expect(recursiveFibonacci(1)).toBe(1);
-//   expect(recursiveFibonacci(2)).toBe(1);
-//   expect(recursiveFibonacci(3)).toBe(2);
-//   expect(recursiveFibonacci(4)).toBe(3);
-//   expect(recursiveFibonacci(5)).toBe(5);
-//   expect(recursiveFibonacci(6)).toBe(8);
-//   expect(recursiveFibonacci(7)).toBe(13);
-//   expect(recursiveFibonacci(8)).toBe(21);
-// });
+describe("Test for recursive Fibonacci", () => {
+  test("Fibonacci number for n = 0", () => {
+    expect(recursiveFibonacci(0)).toBe(0);
+  });
 
-test('Fibonacci number for n = 0', () => {
-  expect(recursiveFibonacci(0)).toBe(0);
-});
+  test("Fibonacci number for n = 1", () => {
+    expect(recursiveFibonacci(1)).toBe(1);
+  });
 
-test('Fibonacci number for n = 1', () => {
-  expect(recursiveFibonacci(1)).toBe(1);
-});
+  test("Fibonacci number for n = 2", () => {
+    expect(recursiveFibonacci(2)).toBe(1);
+  });
 
-test('Fibonacci number for n = 10', () => {
-  expect(recursiveFibonacci(10)).toBe(55);
+  test("Fibonacci number for n = 3", () => {
+    expect(recursiveFibonacci(3)).toBe(2);
+  });
+
+  test("Fibonacci number for n = 4", () => {
+    expect(recursiveFibonacci(4)).toBe(3);
+  });
+
+  test("Fibonacci number for n = 8", () => {
+    expect(recursiveFibonacci(8)).toBe(21);
+  });
 });
